70f6f6c83783b7b970bc266629f56fae6342c21d
Joscha Henningsen <joscha.henningsen@tum.de>
2022-02-16 10:15:00 +0100

    less cooldown, hide like btn

--- model/chat.go
+++ model/chat.go
@@ -38,7 +38,7 @@ var (
 const (
 	maxMessageLength = 250
 	coolDown         = time.Minute * 2
-	coolDownMessages = 5 // 5 messages -> 5 messages per 2 minutes max
+	coolDownMessages = 15 // 15 messages -> 15 messages per 2 minutes max
 )
 
 type Chat struct {
